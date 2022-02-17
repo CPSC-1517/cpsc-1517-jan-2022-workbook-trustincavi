4f4d0838da7dad98bca4f18d092aed9da7200c65
Tri Tin Tran <trustincavi@gmail.com>
2022-02-17 08:40:07 -0700

    Initial classes

---
+++ CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Samples
+{
+    public class BasicDataManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
