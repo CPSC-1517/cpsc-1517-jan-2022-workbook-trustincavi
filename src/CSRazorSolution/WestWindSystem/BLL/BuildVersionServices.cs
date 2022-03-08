91ceb3ca9d2ad4b7d19ddcd5ab98036ee7239c58
Tri Tin Tran <trustincavi@gmail.com>
2022-03-08 08:59:37 -0700

    BuildVersion BLL & register services

---
+++ src/CSRazorSolution/WestWindSystem/BLL/BuildVersionServices.cs
@@ -0,0 +1,45 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional namespaces
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+#endregion
+
+namespace WestWindSystem.BLL
+{
+    public class BuildVersionServices
+    {
+        #region Setup of the context connection variable and class constructor
+        // Variable to hold an instance of context class
+        private readonly WestWindContext _context;
+
+        // Constructor to create an instance of the registered context class
+        internal BuildVersionServices(WestWindContext regcontext)
+        {
+            _context = regcontext;
+        }
+        #endregion
+
+
+        #region Services: Query
+        // Create a method (services) that will retrieve the BuildVersion record.
+        // This will be called from the webapp (PageModel file), thus is needs to be public.
+        // This becomes part of the class library's (application library) public interface.
+        public BuildVersion GetBuildVersion()
+        {
+            // Going to context instance (class), use the property (DbSet)
+            // for BuildVersion data.
+            // Using this property will retrieve data from the database.
+            // The query will get the first record and return it.
+            // If no data, the return value will be null.
+            BuildVersion result = _context.BuildVersions.FirstOrDefault();
+            return result;
+        }
+        #endregion
+    }
+}
--- src/CSRazorSolution/WestWindSystem/BackendExtensions.cs
+++ src/CSRazorSolution/WestWindSystem/BackendExtensions.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 #region Additional Namespaces
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using WestWindSystem.BLL;
 using WestWindSystem.DAL;
 #endregion
 
@@ -23,6 +24,24 @@ namespace WestWindSystem
 
             // Setup the context service
             services.AddDbContext<WestWindContext>(options);
+
+            // Register the service classes
+
+            // Add any business logic layer to the service collection so our
+            // webapp has access to the methods (services) within the BLL class.
+
+            // The argument for the AddTransient is called a factory
+            // Basically what you are add is a localize method.
+            services.AddTransient<BuildVersionServices>((serviceProvider) =>
+            {
+                // Get the dbcontext class that has been registered.
+                var context = serviceProvider.GetService<WestWindContext>();
+
+                // Create an instance of the service class (BuildVersionServices) supplying
+                // the context reference to the service class.
+                // return the service class instance
+                return new BuildVersionServices(context);
+            });
         }
     }
 }
